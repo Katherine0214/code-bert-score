ea8b41b5ee9857530301ae91d544dc210e16a80d
Katherine0214 <46698820+Katherine0214@users.noreply.github.com>
2023-07-05 15:59:56 +0800

    Update demo_chatglm.py

--- LLM_testdemo/demo_chatglm.py
+++ LLM_testdemo/demo_chatglm.py
@@ -9,50 +9,43 @@ from transformers import AutoTokenizer, AutoModel
 import torch
 import linecache
 import xlwt
+import pandas as pd
+import xlrd
+from xlutils import copy
 
-################################################## CodeCompletion ##############################################
-device = "cuda" if torch.cuda.is_available() else "cpu"
-torch.cuda.set_device(1)
-
-
-filename = 'query.txt'
-file = open(filename,'r',encoding ='utf-8') 
-countline_query = len(file.readlines())
-
-pred = xlwt.Workbook()
-pred_sheet = pred.add_sheet('Sheet1')
-myStyle = xlwt.easyxf('font: name Times New Roman, color-index red, bold on', num_format_str='#,##0.00')
-
-tokenizer = AutoTokenizer.from_pretrained("../chatglm/chatglm-6b", trust_remote_code=True)
-model = AutoModel.from_pretrained("../chatglm/chatglm-6b", trust_remote_code=True).half().to(device)
+# ################################################## CodeCompletion ##############################################
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+# torch.cuda.set_device(1)
 
 
-task = "Please complete the following code and give only the next line of code: "
-for i in range(1, countline_query + 1):
-    code_query = linecache.getline(filename, i)
-    query =  task + code_query 
-    response, history = model.chat(tokenizer, query, history=[])    
-    print(response)
+# filename = 'query.txt'
+# file = open(filename,'r',encoding ='utf-8') 
+# countline_query = len(file.readlines())
+
+# pred = xlwt.Workbook()
+# pred_sheet = pred.add_sheet('Sheet1')
+# myStyle = xlwt.easyxf('font: name Times New Roman, color-index red, bold on', num_format_str='#,##0.00')
+
+# tokenizer = AutoTokenizer.from_pretrained("../chatglm/chatglm-6b", trust_remote_code=True)
+# model = AutoModel.from_pretrained("../chatglm/chatglm-6b", trust_remote_code=True).half().to(device)
+
+
+# task = "Please complete the following code and give only the next line of code: "
+# for i in range(1, countline_query + 1):
+#     code_query = linecache.getline(filename, i)
+#     query =  task + code_query 
+#     response, history = model.chat(tokenizer, query, history=[])    
+#     print(response)
     
-    pred_sheet.write(i,0,response)  
-pred.save('pred.xls')     
-
-
-
- 
+#     pred_sheet.write(i,0,response)  
+# pred.save('pred.xls')     
 
 
 
 
 
 
-
-
-
-
-
-
-# ################################################## CodeSummarization ##############################################
+# ################################################## CodeSummarization TXT文件##############################################
 # device = "cuda" if torch.cuda.is_available() else "cpu"
 # torch.cuda.set_device(1)
 
@@ -78,3 +71,35 @@ pred.save('pred.xls')
 # pred.close()    
 
 
+
+
+
+
+################################################## CodeSummarization Excel文件##############################################
+device = "cuda" if torch.cuda.is_available() else "cpu"
+torch.cuda.set_device(0)
+
+xls = xlrd.open_workbook('query.xls')
+sheet = xls.sheet_by_name('Sheet1')
+nrows = sheet.nrows
+
+wbook = copy.copy(xls) #复制文件并保留格式
+wsheet = wbook.get_sheet(0) #打开表单
+
+
+tokenizer = AutoTokenizer.from_pretrained("../chatglm/chatglm2-6b", trust_remote_code=True)
+model = AutoModel.from_pretrained("../chatglm/chatglm2-6b", trust_remote_code=True).half().to(device)
+
+
+task = "Please give a short description of all the functions of the following code in no more than 100 words in English: "
+for i in range(1, nrows):    
+    code_query = sheet.cell_value(i, 1)        # 1代表code在第2列
+    query =  task + code_query 
+    response, history = model.chat(tokenizer, query, history=[])    
+    print(response)
+    
+    wsheet.write(i,7,response)    # 7代表comment放在第8列
+wbook.save('add_comment.xls')    
+
+
+ 
