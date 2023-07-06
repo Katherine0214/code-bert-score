744c85dc1fb3b9012f535108e556713a475c488e
Katherine0214 <46698820+Katherine0214@users.noreply.github.com>
2023-07-06 11:14:38 +0800

    Update demo_chatglm.py

--- LLM_testdemo/demo_chatglm.py
+++ LLM_testdemo/demo_chatglm.py
@@ -42,6 +42,16 @@ from xlutils import copy
 
 
 
+ 
+
+
+
+
+
+
+
+
+
 
 
 
@@ -75,11 +85,69 @@ from xlutils import copy
 
 
 
-################################################## CodeSummarization Excel文件##############################################
+# ################################################## CodeSummarization Excel文件##############################################
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+# torch.cuda.set_device(0)
+
+# xls = xlrd.open_workbook('query.xls')
+# sheet = xls.sheet_by_name('Sheet1')
+# nrows = sheet.nrows
+
+# wbook = copy.copy(xls) #复制文件并保留格式
+# wsheet = wbook.get_sheet(0) #打开表单
+
+
+# tokenizer = AutoTokenizer.from_pretrained("../chatglm/chatglm2-6b", trust_remote_code=True)
+# model = AutoModel.from_pretrained("../chatglm/chatglm2-6b", trust_remote_code=True).half().to(device)
+
+
+# task = "Please give a short description of all the functions of the following code in no more than 100 words in English: "
+# for i in range(1, nrows):    
+#     code_query = sheet.cell_value(i, 1)        # 1代表code在第2列
+#     query =  task + code_query 
+#     response, history = model.chat(tokenizer, query, history=[])    
+#     print(response)
+    
+#     wsheet.write(i,7,response)    # 7代表comment放在第8列
+# wbook.save('add_comment.xls')    
+
+
+
+# ################################################## CodeSummarization CSV文件##############################################
+# device = "cuda" if torch.cuda.is_available() else "cpu"
+# torch.cuda.set_device(0)
+
+# csv = pd.read_csv('code.csv')
+# csv_rows = len(csv)   # 读取csv文件行数
+
+
+# tokenizer = AutoTokenizer.from_pretrained("../chatglm/chatglm2-6b", trust_remote_code=True)
+# model = AutoModel.from_pretrained("../chatglm/chatglm2-6b", trust_remote_code=True).half().to(device)
+
+
+# task = "Please give a short description of all the functions of the following code in no more than 100 words in English: "
+# response_all = []
+# for i in range(0, csv_rows):    
+#     code_query = csv.iloc[i, 1]        # 1代表code在第2列
+#     query =  task + code_query 
+#     response, history = model.chat(tokenizer, query, history=[])    
+#     print(response)
+    
+#     response_all.append(response)
+
+# dataframe = pd.DataFrame({'comment':response_all})
+# #将DataFrame存储为csv,index表示是否显示行名，default=True
+# dataframe.to_csv("add_comment.csv")
+
+ 
+ 
+ 
+ 
+################################################## 实体关系抽取 Excel文件##############################################
 device = "cuda" if torch.cuda.is_available() else "cpu"
 torch.cuda.set_device(0)
 
-xls = xlrd.open_workbook('query.xls')
+xls = xlrd.open_workbook('OTA.xls')
 sheet = xls.sheet_by_name('Sheet1')
 nrows = sheet.nrows
 
@@ -91,15 +159,20 @@ tokenizer = AutoTokenizer.from_pretrained("../chatglm/chatglm2-6b", trust_remote
 model = AutoModel.from_pretrained("../chatglm/chatglm2-6b", trust_remote_code=True).half().to(device)
 
 
-task = "Please give a short description of all the functions of the following code in no more than 100 words in English: "
+task1 = "请抽取下面句子中的实体: "
+task2 = "请给出上述实体两两之间的关系: "
+
 for i in range(1, nrows):    
-    code_query = sheet.cell_value(i, 1)        # 1代表code在第2列
-    query =  task + code_query 
-    response, history = model.chat(tokenizer, query, history=[])    
-    print(response)
+    summary = sheet.cell_value(i, 1)        # 1代表code在第2列
+    content = sheet.cell_value(i, 2) 
+    instance_quary = summary + ". " + content
+    query1 =  task1 + instance_quary 
+    instance_response, history = model.chat(tokenizer, query1, history=[])    
+    wsheet.write(i,3,instance_response)    
     
-    wsheet.write(i,7,response)    # 7代表comment放在第8列
-wbook.save('add_comment.xls')    
-
-
- 
+    query2 =  instance_quary + instance_response + task2 
+    relationship_response, history = model.chat(tokenizer, query2, history=[]) 
+    wsheet.write(i,4,relationship_response)
+    
+    print(i)
+wbook.save('OTA.xls')    
